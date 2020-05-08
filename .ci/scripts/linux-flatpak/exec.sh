3266baa86080550b4ec0a338111a7dd0f9f0098d
Roman Meier <meirodev@outlook.com>
2020-05-08 19:46:23 +0200

    ci: Fix #3895 for linux-flatpak

--- .ci/scripts/linux-flatpak/exec.sh
+++ .ci/scripts/linux-flatpak/exec.sh
@@ -1,6 +1,10 @@
 #!/bin/bash -ex
 mkdir -p "$HOME/.ccache"
 
-# Configure docker and call the script that generates application data and build scripts
+chmod a+x ./.ci/scripts/linux-flatpak/docker.sh
+
+# the UID for the container yuzu user is 1027
+sudo chown -R 1027 ./
 docker run --env-file .ci/scripts/linux-flatpak/azure-ci.env --env-file .ci/scripts/linux-flatpak/azure-ci-flatpak.env -v $(pwd):/yuzu -v "$HOME/.ccache":/root/.ccache -v "$HOME/.ssh":/root/.ssh --privileged meirod/build-environments:linux-flatpak /bin/bash -ex /yuzu/.ci/scripts/linux-flatpak/docker.sh
+sudo chown -R $UID ./
 
