31e18ff80d17e40325960ced1feb68a2f91e5480
Roman Meier <meirodev@outlook.com>
2020-05-09 13:45:27 +0200

    Testing: ensure user .ssh is mounted in docker

--- .ci/scripts/linux-flatpak/docker.sh
+++ .ci/scripts/linux-flatpak/docker.sh
@@ -20,8 +20,6 @@ tar -C /tmp -xvf $KEYS_ARCHIVE
 
 # Configure SSH keys
 eval "$(ssh-agent -s)"
-mkdir "$HOME/.ssh"
-touch "$HOME/.ssh/known_hosts"
 chmod -R 600 "$HOME/.ssh"
 chown -R yuzu "$HOME/.ssh"
 chmod 600 "$SSH_KEY"
--- .ci/scripts/linux-flatpak/exec.sh
+++ .ci/scripts/linux-flatpak/exec.sh
@@ -5,6 +5,6 @@ chmod a+x ./.ci/scripts/linux-flatpak/docker.sh
 
 # the UID for the container yuzu user is 1027
 sudo chown -R 1027 ./
-docker run --env-file .ci/scripts/linux-flatpak/azure-ci.env --env-file .ci/scripts/linux-flatpak/azure-ci-flatpak.env -v $(pwd):/yuzu -v "$HOME/.ccache":/root/.ccache -v "$HOME/.ssh":/root/.ssh --privileged meirod/build-environments:linux-flatpak /bin/bash -ex /yuzu/.ci/scripts/linux-flatpak/docker.sh
+docker run --env-file .ci/scripts/linux-flatpak/azure-ci.env --env-file .ci/scripts/linux-flatpak/azure-ci-flatpak.env -v $(pwd):/yuzu -v "$HOME/.ccache":/home/yuzu/.ccache -v "$HOME/.ssh":/home/yuzu/.ssh --privileged meirod/build-environments:linux-flatpak /bin/bash -ex /yuzu/.ci/scripts/linux-flatpak/docker.sh
 sudo chown -R $UID ./
 
